3e692450e34ea5041d88c4ba1c21d69936697930
Maxim1321 <150693068+Maxim1321@users.noreply.github.com>
2023-12-17 20:28:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace Работаем_в_VS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+            Console.ReadLine();
+        }
+    }
+}
